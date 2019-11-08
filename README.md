51691698117f7ca37dd7788cfcbe32f9f9ece453
Christopher <chris@christophermoore.website>
2019-11-08 23:38:55 +1000

    Public Beta Version 1.2
    Co-Authored-By: Daniel Hawton <daniel@hawton.org>
    Co-Authored-By: scottuk <scottuk@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# Police-Panic
-A simple standalone script that adds Panic Buttons to FiveM.
+![channels4_banner|690x114](https://i.ibb.co/CHMD8y6/channels4-banner.jpg) 
+# Inferno Collection: Pass Alarm
+[![Build Status](https://travis-ci.com/inferno-collection/Police-Panic.svg?branch=master)](https://travis-ci.com/inferno-collection/Police-Panic)
+
+__Public Beta Version 1.2 Beta__
+
+The Police Panic script is a simple Standalone script that allows Officers to tune into a "Panic Channel", which allows them to receive Panic Button activations, as well as activate their own Panic Button. Once a Panic Button is activated, all tuned Officers will hear an Emergency tone, followed by the name of the Officer, their nearest street and cross street, as well as a large flashing blip on their map, with a flashing map route; these will only display for a few seconds.
+
+Presently, the following can be customized:
+- Cooldown time between activations
+- Chat Messages
+- Toggleable notifications and chat suggestions
+- Which players can use which command (via a whitelist).
+- Choice of three whitelist options:
+    1. Steam ID based JSON file
+    2. Ace Permissions
+    3. No whitelist
+
+***
+### Development Showcase Video
+[![Watch the Development Showcase video](https://img.youtube.com/vi/-usFNl5YXb4/maxresdefault.jpg)](https://www.youtube.com/watch?v=-usFNl5YXb4)
+***
+
+> Download Link
+
+https://github.com/inferno-collection/Police-Panic/releases
+
+> Pictures
+
+![SPOILER-unknown|671x328](https://i.ibb.co/HCDXY8B/SPOILER-unknown.png) 
+
+***
+Check out the Wiki for an [installation guide](https://github.com/inferno-collection/Police-Panic/wiki/Installation-Guide), as well as to find out how to use the resource.
+
+If you have any troubles, suggestions, feedback, etc, please [check the Wiki](https://github.com/inferno-collection/Police-Panic/wiki) and/or [create a new issue](https://github.com/inferno-collection/Police-Panic/issues/new/choose) on GitHub.
+
+Thank you to our Alpha Testers, and their respective servers and players, for testing this resource:
+* LurkingSpirit#8338
+* Tom | DutchGamingsHD15#7193
+* J. Hughesman#6922
+* Ovist#9081
+* FamalamJamalam#0001
+* Beerasaurus - Rex#9371
+* Trevor Cory#7451
+
+> The Inferno Collection Team
+* @ChristopherM
+* @Scott_UK 
+* @FrozenN00b
+***
+Interested in keeping up-to-date with what we are working on? [Check out our roadmap](https://inferno-collection.com/roadmap).
---
+++ [inferno-collection]/inferno-police-panic/server.lua
@@ -0,0 +1,145 @@
+-- Inferno Collection Police Panic Version 1.2 Beta
+--
+-- Copyright (c) 2019, Christopher M, Inferno Collection. All rights reserved.
+--
+-- This project is licensed under the following:
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to use, copy, modify, and merge the software, under the following conditions:
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. THE SOFTWARE MAY NOT BE SOLD.
+--
+
+-- Play tones on all clients
+RegisterServerEvent("Police-Panic:NewPanic")
+AddEventHandler("Police-Panic:NewPanic", function(Officer)
+	TriggerClientEvent("Pass-Alarm:Return:NewPanic", -1, source, Officer)
+end)
+
+-- Whitelist check on server join
+RegisterServerEvent("Police-Panic:WhitelistCheck")
+AddEventHandler("Police-Panic:WhitelistCheck", function(Whitelist)
+	for i in pairs(Whitelist.Command) do
+		Whitelist.Command[i] = "pending"
+	end
+
+	-- If usin json file as whitelist
+	if Whitelist.Enabled:lower() == "json" then
+		-- Collect all the data from the whitelist.json file
+		local Data = LoadResourceFile(GetCurrentResourceName(), "whitelist.json")
+		-- If able to collect data
+		if Data then
+			-- Place the decoded whitelist into the array
+			local Entries = json.decode(Data)
+
+			-- Loop through the whitelist array
+			for _, Entry in ipairs(Entries) do
+				-- Check if the player exists in the array.
+				if GetPlayerIdentifier(source):lower() == Entry.steamhex:lower() then
+					-- Loop though all values in whitelist entry
+					for i in pairs(Entry) do
+						-- If the value is not the player's steam hex
+						if i ~= "steamhex" then
+							-- If whitelist value is true, aka they have access to a command
+							if Entry[i] then
+								-- If command is a valid command
+								if Whitelist.Command[i] then
+									-- Allow player to use that command
+									Whitelist.Command[i] = true
+								-- If command is not valid
+								else
+									-- Print error message to server console
+									print("===================================================================")
+									print("==============================WARNING==============================")
+									print("/" .. i .. " is not a valid command, but is listed in ")
+									print(Entry.steamhex:lower() .. "'s whitelist entry. Please correct this")
+									print("issue, and reload the whitelist with /panicwhitelist reload.")
+									print("Note: Entries are CaSe SeNsItIvE.")
+									print("===================================================================")
+								end
+							end
+						end
+					end
+					-- Break the loop once whitelist entry found
+					break
+				end
+			end
+		-- If unable to load json file
+		else
+			-- Print error message to server console
+			print("===================================================================")
+			print("==============================WARNING==============================")
+			print("Unable to load whitelist file for Inferno-Police-Panic. The white")
+			print("list has been disabled. This message will appear every time someone")
+			print("joins the server until the issue is corrected.")
+			print("===================================================================")
+			-- Loop through all commands
+			for i in pairs(Whitelist.Command) do
+				-- Grant players all permissions
+				Whitelist.Command[i] = true
+			end
+			-- Override whitelist permission
+			Whitelist.Command.panicwhitelist = false
+		end
+
+		-- Loop through all commands
+		for i in pairs(Whitelist.Command) do
+			-- If command is still pending
+			if Whitelist.Command[i] == "pending" then
+				-- Deny access
+				Whitelist.Command[i] = false
+			end
+		end
+	-- If using Ace permissions
+	elseif Whitelist.Enabled:lower() == "ace" then
+		-- Loop through all commands
+		for i in pairs(Whitelist.Command) do
+			-- Grant player permission to command based on Ace group
+			Whitelist.Command[i] = IsPlayerAceAllowed(source, "Police-Panic." .. i)
+		end
+	-- If using neither json, Ace, or disabled
+	else
+		-- Print error message to server console
+		print("===================================================================")
+		print("==============================WARNING==============================")
+		print("''" .. tostring(Whitelist.Enabled) .. "'' is not a valid Whitelist option.")
+		print("The whitelist has been disabled.")
+		print("===================================================================")
+		-- Loop through all commands
+		for i in pairs(Whitelist.Command) do
+			-- Grant players all permissions
+			Whitelist.Command[i] = true
+		end
+		-- Override whitelist permission
+		Whitelist.Command.panicwhitelist = false
+	end
+	-- Return whietlist object to client
+	TriggerClientEvent("Police-Panic:Return:WhitelistCheck", source, Whitelist)
+end)
+
+-- Whitelist reload on all clients
+RegisterServerEvent("Police-Panic:WhitelistReload")
+AddEventHandler("Police-Panic:WhitelistReload", function()
+	-- Bounce to all clients
+	TriggerClientEvent("Police-Panic:WhitelistRecheck", -1)
+end)
+
+-- Add entry to whitelist (json only)
+RegisterServerEvent("Police-Panic:WhitelistAdd")
+AddEventHandler("Police-Panic:WhitelistAdd", function(ID, Entry)
+	-- Collect all the data from the whitelist.json file
+	local Data = json.decode(LoadResourceFile(GetCurrentResourceName(), "whitelist.json"))
+
+	-- If 'steam hex' provided was a number
+	if tonumber(ID) then
+		-- Get steam hex based off of number
+		ID = GetPlayerIdentifier(ID)
+	end
+
+	-- Add the steam hex to the whitelist entry
+	Entry.steamhex = ID
+	-- Add the entry to the existing whitelist
+	table.insert(Data, Entry)
+	-- Covert the entire object to a json format, then save it over the existing file
+	SaveResourceFile(GetCurrentResourceName(), "whitelist.json", json.encode(Data), -1)
+	-- Force all clients to reload their whitelists
+	TriggerClientEvent("Police-Panic:WhitelistRecheck", -1)
+end)
